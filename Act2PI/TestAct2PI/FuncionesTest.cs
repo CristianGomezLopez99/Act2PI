c731cdc4424c8c2137fc451f8448a54d3ee42797
alegimlop <alegimlop@alu.edu.gva.es>
2025-10-24 00:19:22 +0200

    cración test de la función 1

--- Act2PI/TestAct2PI/FuncionesTest.cs
+++ Act2PI/TestAct2PI/FuncionesTest.cs
@@ -4,7 +4,23 @@ namespace TestAct2PI
     [TestClass]
     public class FuncionesTest
     {
-        
-        
+        [TestMethod]
+        public void Es_Negativo()
+        {
+            long result = Funciones.CalcularFactorial(-1);
+            Assert.AreEqual(-1, result);
+        }
+        [TestMethod]
+        public void Es_Cero()
+        {
+            long result = Funciones.CalcularFactorial(0);
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void Es_Positivo()
+        {
+            long result = Funciones.CalcularFactorial(6);
+            Assert.AreEqual(720, result);
+        }
     }
 }
