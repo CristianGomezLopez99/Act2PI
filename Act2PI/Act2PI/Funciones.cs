536726f7a175f949d79ff7521a2695007266d4f7
CristianGomezLopez99 <crigomlop3@alu.edu.gva.es>
2025-10-24 00:20:01 +0200

    creacion funcion 1 (factorial)

--- Act2PI/Act2PI/Funciones.cs
+++ Act2PI/Act2PI/Funciones.cs
@@ -8,6 +8,23 @@ namespace Act2PI
 {
     public class Funciones
     {
-    
+       public static long CalcularFactorial(int num)
+        {
+            if (num < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                long resultado = 1;
+                for (int i = 1; i <= num; i++)
+                {
+                    resultado *= i;
+                }
+                return resultado;
+            }
+
+        }
     }
 }
+    
